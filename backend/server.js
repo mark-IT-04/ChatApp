45e494a6eb388169b3ab985999f008147d5944c1
mark_it_04 <markit1421@gmail.com>
2023-03-25 04:32:07 +0800

    initial set-up

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+
+
+dotenv.config()
+
+connectDB()
+
+const app= express() 
+
+app.use(express.json())
+
+
+app.get('/', (req,res)=>{
+    res.send('API is running...')
+})
+
+
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT=process.env.PORT
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`))
